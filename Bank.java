fd69b5fd1bbb81070b9fc0f4c0a5e40351901af2
P4na <vip_P4na@protonmail.com>
2022-10-19 16:40:15 +0200

    Aggiunto elementi

---
+++ Bank.java
@@ -0,0 +1,5 @@
+public class Bank {
+    String nomeBanca;
+    Cliente[] clienti;
+    Conto[] contiCorrenti;
+}
---
+++ Cliente.java
@@ -0,0 +1,5 @@
+public class Cliente {
+    String nomeCliente;
+    int numeroTelefono;
+
+}
