cfea06beef2004f8f83acb67cfd7bdfe430c34e2
P4na <vip_P4na@protonmail.com>
2022-10-19 16:40:23 +0200

    Aggiunto elementi

---
+++ Conto.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Conto {
+    int numeroConto;
+    double saldo = 0;
+    ArrayList<String> operazioniEffetuate;
+
+    public Conto(int numeroConto, double saldo) {
+        this.numeroConto = numeroConto;
+        this.saldo = saldo;
+    }
+
+
+}
